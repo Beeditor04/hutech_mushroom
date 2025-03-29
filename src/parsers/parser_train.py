95a0bd03bb729109a7f2bff6181d994aaa24c9f9
Beeditor04 <nhnam.beeditor@gmail.com>
2025-03-29 10:19:57 +0700

    remove config path

--- src/parsers/parser_train.py
+++ src/parsers/parser_train.py
@@ -3,7 +3,7 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser(description="Train with different models.")
     parser.add_argument("--model", default="alexnet", type=str, help="Model name to train")
-    parser.add_argument("--config", default="../config/exp.yaml", type=str, help="Path to the config file")
+    parser.add_argument("--config", type=str, help="Path to the config file")
     parser.add_argument("--project", default="hutech_mushroom", type=str, help="WandB project name")
     parser.add_argument("--dataset", default="hutech-dataset:latest", type=str, help="Dataset name")
     return parser
