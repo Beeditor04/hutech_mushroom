998b16ea364b2094ec03dfab01656d029c6f7bf9
Beeditor04 <nhnam.beeditor@gmail.com>
2025-03-29 08:43:02 +0700

    helper function

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -4,15 +4,41 @@ from torch.utils.data import DataLoader
 
 import os 
 
-def get_data_loader(dir, batch_size, type):
-    base_transform = transforms.Compose([
-        transforms.Resize(227),
-        transforms.CenterCrop(227),
-        transforms.ToTensor(),
-    ])
-    df_path = os.path.join(dir, type)
-    df = datasets.ImageFolder(df_path, transform=base_transform)  
+def get_data_loader(dir, config, mode):
+    transform_list = []
 
-    loader = DataLoader(df, batch_size=batch_size, shuffle=True)
+    transform_list.append(transforms.Resize(config['resize']))
+    
+    if config.get("horizontal_flip", 0):
+        transform_list.append(transforms.RandomHorizontalFlip(p=config['horizontal_flip']))
+    
+    if config.get("vertical_flip", 0):
+        transform_list.append(transforms.RandomVerticalFlip(p=config['vertical_flip']))
+    
+    if config.get("random_rotation", 0):
+        transform_list.append(transforms.RandomRotation(config['random_rotation']))
+        
+    if any(config.get(key, 0) for key in ['brightness', 'contrast', 'saturation', 'hue']):
+        transform_list.append(transforms.ColorJitter(
+            brightness=config.get('brightness', 0),
+            contrast=config.get('contrast', 0),
+            saturation=config.get('saturation', 0),
+            hue=config.get('hue', 0)
+        ))
+    
+    if config.get("sigma", 0) or config.get("kernel_size", 0):
+        transform_list.append(transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2)))
+    
+    transform_list.append(transforms.ToTensor())
+    # convert from list to transforms
+    base_transform = transforms.Compose(transform_list)
+    if mode in ("test", "val"):
+        base_transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor()
+        ])
+    df_path = os.path.join(dir, mode)
+    dataset = datasets.ImageFolder(df_path, transform=base_transform)  
+    loader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
 
     return loader
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -4,3 +4,15 @@ def load_config(path):
     with open(path, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
+
+from models.mini_alexnet import MiniAlexNet
+from models.alexnet import AlexNet
+
+def get_model(name, num_classes):
+    if name == "mini_alexnet":
+        model = MiniAlexNet(num_classes=num_classes)
+    elif name == "alexnet":
+        model = AlexNet(num_classes=num_classes)
+    else:
+        raise ValueError(f"Model {name} not supported.")
+    return model
