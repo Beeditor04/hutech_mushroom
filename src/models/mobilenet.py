9514eee8eadefc8df3fcb7fa383ec2435069a344
Beeditor04 <nhnam.beeditor@gmail.com>
2025-04-01 19:32:48 +0700

    fix mobilenet

--- src/models/mobilenet.py
+++ src/models/mobilenet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torchvision import models
 
 class MobileNetV3(nn.Module):
-    def __init__(self, num_classes=4, freeze=False, pretrained=True):
+    def __init__(self, num_classes=4, freeze=False, pretrained=True, include_top=True):
         super(MobileNetV3, self).__init__()
         if pretrained:
             self.model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1)
@@ -11,6 +11,8 @@ class MobileNetV3(nn.Module):
             self.model = models.mobilenet_v3_small(weights=None)
         in_features = self.model.classifier[3].in_features
         self.model.classifier[3] = nn.Linear(in_features, num_classes)
+        if not include_top:
+            self.model.classifier[3] = nn.Identity()
         if freeze and pretrained:
             for param in self.model.parameters():
                 param.requires_grad = False
